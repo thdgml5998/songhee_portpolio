4268d412251dcc7351166f8562f3f616b0503a97
thdgml5998 <152259457+thdgml5998@users.noreply.github.com>
2024-01-09 15:59:14 +0900

    Add files via upload

---
+++ kohome/script/script.js
@@ -0,0 +1,115 @@
+$(document).ready(function(){
+    layout();
+
+    window.addEventListener("resize",()=>{
+        history.go(0);
+        layout();
+    })
+
+    // 2차메뉴
+    $(".top_menu").mouseenter(function(){
+        $(".sub_menu",this).stop().slideDown(500);
+    })
+    $(".top_menu").mouseleave(function(){
+        $(".sub_menu",this).stop().slideUp(500);
+    })
+
+    //배너 스와이퍼
+    const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next', 
+            prevEl: '.swiper-button-prev'
+        }
+    })
+
+    //하단 자동 슬라이드배너
+    let auto_slide=setInterval(function(){
+        slide_width=$(".slide").width();
+        $(".slide_wrap").animate({
+            left:-slide_width
+        },5000,function(){
+            $(".slide").eq(0).appendTo(".slide_wrap");
+            $(".slide_wrap").css({
+                left:0
+            })
+        })
+    })
+
+
+
+
+    function layout(){
+        ww=$(window).width();
+
+        $(".wrap").css({
+            width:ww
+        })
+        $(".w_width").css({
+            width:"100%"
+        })
+        $(".main_header").css({
+            height:ww*0.05
+        })
+        $(".side_box").css({
+            top:ww*0.05+ww*0.03+70
+        })
+        $(".cont_top").css({
+            height:ww*0.03
+        })
+        $(".cont_1").css({
+            height:ww*0.35
+        })
+        $(".cont_2").css({
+            height:ww*0.16
+        })
+        $(".swiper").css({
+            marginLeft:0
+        })
+        $(".cont_3").css({
+            height:ww*0.15
+        })
+        $(".cont_4").css({
+            height:ww*0.15
+        })
+
+
+        $(".sub_banner").css({
+            height:ww*0.05
+        })
+        $(".main_footer").css({
+            height:ww*0.1
+        })
+    
+        
+        
+
+
+        if(ww<1200 && ww>=860){
+
+        }else if(ww<860){
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
